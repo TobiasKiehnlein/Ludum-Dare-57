80dcfed9a2321f5b1809360e2ba5fe9aedb87c7e
Tobias Kiehnlein <tobias@kiehnlein.com>
2025-04-06 15:28:59 +0200

    Fixed flickering

--- godot/Scripts/Flickering.gd
+++ godot/Scripts/Flickering.gd
@@ -1,11 +1,23 @@
 extends OmniLight3D
 
-@export var noise: NoiseTexture2D
+@export var flickeringSpeed: float = 5
+
 var time_passed = 0.0
+
+var texture: NoiseTexture2D
+var initialEnergy: float
+var offset: float
+
+func _init() -> void:
+	texture = NoiseTexture2D.new()
+	texture.noise = FastNoiseLite.new()
+	initialEnergy = light_energy
+	offset = randf_range(0,100)
+
 
 func _process(delta: float) -> void:
 	time_passed += delta
 	
-	var sampled_noise = noise.noise.get_noise_1d(time_passed)
-	sampled_noise = abs(sampled_noise)
-	light_energy = sampled_noise
+	var sampled_noise = texture.noise.get_noise_1d((time_passed+offset)* flickeringSpeed)
+	sampled_noise = clamp( abs(sampled_noise), .2, 1)
+	light_energy = sampled_noise*initialEnergy
