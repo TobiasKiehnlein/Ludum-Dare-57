e2e4f19a7c52b1a93538721d30e1091256f68ebf
Peter Viechter <zelberor@schnalzerbuam.de>
2025-04-07 16:08:22 +0200

    Add props + Update lighting

--- godot/Scripts/Stairs.gd
+++ godot/Scripts/Stairs.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var player: Node3D = $"/root/Root/Player"
 
 var bottom = 0
+var lights: Array
 
 func instantiateNext():
 	var randi = randi_range(0, len(stairCaseScenes) - 1)
@@ -22,10 +23,48 @@ func instantiateNext():
 func _ready() -> void:
 	assert(len(stairCaseScenes) > 0)
 	#bottom = self.get_children().map(func(child): return child.get_node('Center')).reduce(func(lowest, child): return child.position.y if child.position.y < lowest else lowest, 1000000000)
-	for i in range(10):
+	for i in range(5):
 		instantiateNext()
+	_find_lights()
+
+func _find_lights() -> void:
+	#var children = self.find_children("*", "Light3D", true)
+	lights = []
+	var children = self.get_children()
+	_search_lights_children(children)
+
+func _search_lights_children(children: Array[Node]) -> void:
+	for c in children:
+		if c is OmniLight3D or c is SpotLight3D:
+			lights.append(c)
+		_search_lights_children(c.get_children())
 
 func _process(delta: float) -> void:
+	var remove_freed = func(a) -> bool:
+		if is_instance_valid(a):
+			return true
+		return false
 	
-	if player.position.y - 100 < bottom:
+	lights = lights.filter(remove_freed)
+	
+	var player_dist_sort = func(a: Node3D, b: Node3D) -> bool:
+		var cmp_pos = player.global_position + Vector3.DOWN * 2.5
+		var dist_a = a.global_position.distance_to(cmp_pos)
+		var dist_b = b.global_position.distance_to(cmp_pos)
+		return dist_a < dist_b
+
+	# Only leave 8 Lights enabled based on distance
+	lights.sort_custom(player_dist_sort)
+	var count: int = 0
+	for light in lights:
+		if count < 16:
+			light.show()
+		else:
+			light.hide()
+		count += 1
+		pass
+		
+	
+	if player.position.y - 15 < bottom:
 		instantiateNext()
+		_find_lights()
